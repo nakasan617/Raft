21ae698ba0ca2b366ac9661a3ce1069062fd6855
Yuta Nakamura <yutanak6@gmail.com>
2022-06-01 13:39:41 -0500

    starting leader election

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -4,12 +4,20 @@ import akka.actor.{ActorSystem, Props}
 
 object main extends App {
   val system = ActorSystem("Servers")
-  val node1 = system.actorOf(Props[Server](), name = "node1")
-  val node2 = system.actorOf(Props[Server](), name = "node2")
-  val node3 = system.actorOf(Props[Server](), name = "node3")
+  val node1 = system.actorOf(Props(classOf[Server], "leader"))
+  val node2 = system.actorOf(Props(classOf[Server], "follower"))
+  val node3 = system.actorOf(Props(classOf[Server], "follower"))
+  //val mapActors = context.actorOf(RoundRobinPool(numberMappers).props(Props(classOf[MapActor], reduceActors)))
   node1 ! AddServer(node2)
   node1 ! AddServer(node3)
+  node2 ! AddServer(node3)
+  node2 ! AddServer(node1)
+  node3 ! AddServer(node1)
+  node3 ! AddServer(node2)
+  node3 ! Start
+  node2 ! Start
   node1 ! Start
+
   Thread.sleep(20000)
   System.out.println("system terminating")
   system.terminate()
--- src/main/scala/server.scala
+++ src/main/scala/server.scala
@@ -1,4 +1,4 @@
-import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.actor.{Actor, ActorRef, ActorSystem, Props, Cancellable}
 
 import scala.concurrent.duration._
 import scala.collection.mutable.HashSet
@@ -6,33 +6,80 @@ import akka.util.Timeout
 
 case object Start
 case object HeartBeat
+case object VoteForMe
 case class AddServer(server: ActorRef)
 
-class Server extends Actor {
+class Server(var role: String) extends Actor {
   var cnt = 0
   val otherServers: HashSet[ActorRef] = new HashSet()
+  var cancellable: Cancellable = null
+  val system = context.system
+  var term: Int = 0
 
   def receive = {
-    case Start => sendHeartBeats()
+    case Start =>
+      if(role == "leader") sendHeartBeats()
+      else waitForHeartBeat()
     case HeartBeat => receiveHeartBeat()
     case AddServer(server: ActorRef) => addServer(server)
+    case VoteForMe => println("vote for me received from :" + sender.path.toString)
   }
 
+  /*
+  this is the method for the leader
+  it sends the heart beat to each followers
+   */
   def sendHeartBeats() = {
-    val system = context.system
     implicit val ec = system.dispatcher
-    system.scheduler.scheduleWithFixedDelay(0.seconds, 300.millis) { () =>
+    cancellable = system.scheduler.scheduleWithFixedDelay(0.seconds, 300.millis) { () =>
       implicit val timeout = Timeout(5.seconds)
       otherServers.foreach((server: ActorRef) => server ! HeartBeat)
     }
   }
 
-  def receiveHeartBeat() = {
-    println("received heartbeat: " + cnt)
-    cnt = cnt + 1
+  /*
+  this method waits for the heart beat for each interval,
+  if it doesn't get the heart beat,
+  it starts the election
+  */
+  def waitForHeartBeat() = {
+    implicit val ec = system.dispatcher
+    cancellable = system.scheduler.scheduleOnce(2.seconds) {
+      implicit val timeout = Timeout(5.seconds)
+      startElection()
+    }
   }
 
+  /*
+  this is what we do when we receive the heart beat,
+  we cancel the scheduled election and start a new schedule
+   */
+  def receiveHeartBeat() = {
+    if(cancellable != null) {
+      cancellable.cancel()
+    } else {
+      println("cancellable was null")
+    }
+//    println("received heartbeat: " + cnt)
+    cnt = cnt + 1
+    implicit val ec = system.dispatcher
+    cancellable = system.scheduler.scheduleOnce(2.seconds) {
+      implicit val timeout = Timeout(5.seconds)
+      startElection()
+    }
+  }
+
+  /*
+  this methods adds other servers
+   */
   def addServer(server: ActorRef) = {
     otherServers += server
+  }
+
+  def startElection() = {
+    println("did not receive the heartBeat, starting leader election: " + self.path.toString)
+    term += 1
+    role = "candidate"
+    otherServers.foreach((server: ActorRef) => server ! VoteForMe)
   }
 }
