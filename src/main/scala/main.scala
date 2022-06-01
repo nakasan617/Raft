6a89018236b3172c8fb017cb61d1b12a25a1db53
Yuta Nakamura <nakamura1@cs.uchicago.edu>
2022-06-01 17:59:37 -0500

    working on leader election

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,5 +1,5 @@
 
-import akka.actor.{ActorSystem, Props}
+import akka.actor.{ActorSystem, Props, PoisonPill}
 //import com.typesafe.config.ConfigFactory
 
 object main extends App {
@@ -7,18 +7,42 @@ object main extends App {
   val node1 = system.actorOf(Props(classOf[Server], "leader"))
   val node2 = system.actorOf(Props(classOf[Server], "follower"))
   val node3 = system.actorOf(Props(classOf[Server], "follower"))
+  val node4 = system.actorOf(Props(classOf[Server], "follower"))
+  val node5 = system.actorOf(Props(classOf[Server], "follower"))
   //val mapActors = context.actorOf(RoundRobinPool(numberMappers).props(Props(classOf[MapActor], reduceActors)))
   node1 ! AddServer(node2)
   node1 ! AddServer(node3)
+  node1 ! AddServer(node4)
+  node1 ! AddServer(node5)
   node2 ! AddServer(node3)
+  node2 ! AddServer(node4)
+  node2 ! AddServer(node5)
   node2 ! AddServer(node1)
+  node3 ! AddServer(node4)
+  node3 ! AddServer(node5)
   node3 ! AddServer(node1)
   node3 ! AddServer(node2)
+  node4 ! AddServer(node5)
+  node4 ! AddServer(node1)
+  node4 ! AddServer(node2)
+  node4 ! AddServer(node3)
+  node5 ! AddServer(node1)
+  node5 ! AddServer(node2)
+  node5 ! AddServer(node3)
+  node5 ! AddServer(node4)
+
+  node5 ! Start
+  node4 ! Start
   node3 ! Start
   node2 ! Start
   node1 ! Start
 
-  Thread.sleep(20000)
-  System.out.println("system terminating")
+  Thread.sleep(2000)
+  println("poisonPill to node1")
+  node1 ! PoisonPill
+  node1 ! "message"
+
+  Thread.sleep(10000)
+  println("system terminating")
   system.terminate()
 }
--- src/main/scala/server.scala
+++ src/main/scala/server.scala
@@ -1,28 +1,42 @@
 import akka.actor.{Actor, ActorRef, ActorSystem, Props, Cancellable}
+import akka.pattern.ask
 
 import scala.concurrent.duration._
+import scala.concurrent.Await
 import scala.collection.mutable.HashSet
 import akka.util.Timeout
 
 case object Start
 case object HeartBeat
-case object VoteForMe
+case object CheckSelf
+case object Vote
 case class AddServer(server: ActorRef)
+case class VoteForMe(theirTerm: Int)
+case class FollowMe(theirTerm: Int, theirRole: String)
+case class LeaderNotify(theirTerm: Int)
 
 class Server(var role: String) extends Actor {
   var cnt = 0
+  val constantTime: Int = 500
   val otherServers: HashSet[ActorRef] = new HashSet()
   var cancellable: Cancellable = null
   val system = context.system
+  val r = scala.util.Random
   var term: Int = 0
+  var numVotes: Int = 0
 
   def receive = {
+    case message: String => println(message)
     case Start =>
       if(role == "leader") sendHeartBeats()
       else waitForHeartBeat()
     case HeartBeat => receiveHeartBeat()
     case AddServer(server: ActorRef) => addServer(server)
-    case VoteForMe => println("vote for me received from :" + sender.path.toString)
+    case VoteForMe(theirTerm: Int) => voteForMe(theirTerm, sender())
+    case CheckSelf => sender() ! "OK"
+    case Vote => voteReceived(sender())
+    case FollowMe(theirTerm: Int, theirRole: String) => followMe(theirTerm, theirRole, sender())
+    case LeaderNotify(theirTerm: Int) => leaderNotify(theirTerm: Int, sender())
   }
 
   /*
@@ -31,9 +45,14 @@ class Server(var role: String) extends Actor {
    */
   def sendHeartBeats() = {
     implicit val ec = system.dispatcher
-    cancellable = system.scheduler.scheduleWithFixedDelay(0.seconds, 300.millis) { () =>
+    cancellable = system.scheduler.scheduleWithFixedDelay(0.seconds, 200.millis) { () =>
       implicit val timeout = Timeout(5.seconds)
       otherServers.foreach((server: ActorRef) => server ! HeartBeat)
+      val future = ask(self, CheckSelf)
+      Await.result(future, 50.millis)
+      if(future == null) {
+        cancellable.cancel()
+      }
     }
   }
 
@@ -43,8 +62,9 @@ class Server(var role: String) extends Actor {
   it starts the election
   */
   def waitForHeartBeat() = {
+    val time = r.nextInt(300) + constantTime
     implicit val ec = system.dispatcher
-    cancellable = system.scheduler.scheduleOnce(2.seconds) {
+    cancellable = system.scheduler.scheduleOnce(time.millis) {
       implicit val timeout = Timeout(5.seconds)
       startElection()
     }
@@ -56,14 +76,16 @@ class Server(var role: String) extends Actor {
    */
   def receiveHeartBeat() = {
     if(cancellable != null) {
+//      println("cancelled at :" + self.path.toString)
       cancellable.cancel()
     } else {
       println("cancellable was null")
     }
 //    println("received heartbeat: " + cnt)
-    cnt = cnt + 1
+//    cnt = cnt + 1
+    val time = r.nextInt(300) + constantTime
     implicit val ec = system.dispatcher
-    cancellable = system.scheduler.scheduleOnce(2.seconds) {
+    cancellable = system.scheduler.scheduleOnce(time.millis) {
       implicit val timeout = Timeout(5.seconds)
       startElection()
     }
@@ -76,10 +98,110 @@ class Server(var role: String) extends Actor {
     otherServers += server
   }
 
+  /*
+  this method is what the follower does when the leader dies and new node has to be elected
+  numVotes starts out as one because you vote for yourself
+   */
   def startElection() = {
-    println("did not receive the heartBeat, starting leader election: " + self.path.toString)
     term += 1
+    println("did not receive the heartBeat, starting leader election: " + self.path.toString + ", now at term " + term)
     role = "candidate"
-    otherServers.foreach((server: ActorRef) => server ! VoteForMe)
+    numVotes = 1
+    otherServers.foreach((server: ActorRef) => server ! VoteForMe(term))
+  }
+
+  def voteForMe(theirTerm: Int, candidate: ActorRef) = {
+    if(role == "follower") {
+      if(theirTerm > term) {
+        if(cancellable != null) {
+          cancellable.cancel()
+        }
+        println("I am " + self.path.toString + ", voting for " + candidate.path.toString + " in term " + theirTerm)
+        candidate ! Vote
+        term = theirTerm
+      } else if(theirTerm == term) {
+        // you already voted for someone
+      } else {
+        // term > theirTerm
+        // I will leave this part out yet
+      }
+    } else if (role == "leader") {
+      if(theirTerm > term) {
+        role = "follower"
+        if(cancellable != null) {
+          cancellable.cancel()
+        }
+        candidate ! Vote
+      } else if(theirTerm < term) {
+        candidate ! FollowMe(term, role)
+      } else {
+        println("The term is the same and you guys are candidate and leader, this can't be happening")
+      }
+    } else {
+      assert(role == "candidate")
+      if(theirTerm == term) {
+        // don't do anything
+      } else if (theirTerm > term) {
+        // I need to follow them
+        role = "follower"
+        term = theirTerm
+        println("term changed at " + self.path.toString() + " at vote for me")
+        candidate ! Vote
+      } else {
+        // they need to follow me
+        assert(theirTerm < term)
+        candidate ! FollowMe(term, role)
+      }
+    }
+  }
+
+  def followMe(theirTerm: Int, theirRole: String, leader: ActorRef): Unit = {
+    assert(theirTerm > term)
+    if(theirRole == "leader") {
+      term = theirTerm
+      role = "follower"
+      receiveHeartBeat()
+    } else if(theirRole == "candidate") {
+      term = theirTerm
+      role = "follower"
+      leader ! Vote
+    } else {
+      println("we are at follow me and ")
+    }
+  }
+
+  def voteReceived(ref: ActorRef): Unit = {
+    numVotes += 1
+    val half: Int = otherServers.size / 2
+    if(numVotes > half) {
+      println("leader elected, " + self.path.toString + " is the new leader at term " + term)
+    }
+    otherServers.foreach((server:ActorRef) => {
+      implicit val timeout = Timeout(3.seconds)
+      val future = ask(server, LeaderNotify).mapTo[String]
+      Await.result(future, timeout.duration)
+      if(future == null || future.value == null) {
+        println("leader notify did not work")
+        system.terminate()
+      } else if(future.value.get.get == "No Good") {
+        println("there is a problem with the term")
+        system.terminate()
+      } else if(future.value.get.get == "Okay") {
+        // don't do anything
+      } else {
+        println("some random message received")
+      }
+    })
+    role = "leader"
+  }
+
+  def leaderNotify(theirTerm: Int, newLeader: ActorRef): Unit = {
+    if(theirTerm >= term) {
+      newLeader ! "OK"
+      term = theirTerm
+      role = "follower"
+    } else {
+      newLeader ! "No Good"
+    }
   }
 }
